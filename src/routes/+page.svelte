4a1afb368d63804205c51484bf46f694aba8d1ec
Luis Flores <luis_dfc_98@hotmail.com>
2025-04-18 22:21:21 -0600

    feature: added styling, todo: scripting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<h3>Buenas noches les hablo desde Svelte</h3>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Luis Flores Portfolio</h1>
+<h3>To know more about me type <span class="command">help</span></h3>
+<div class="redes">
+  <ul>
+    <li>LinkedIn:</li>
+    <li>Github:</li>
+    <li>Email:</li>
+  </ul>
+</div>
+<div class="input">
+  <span>~>></span><textarea name="input" id="input"></textarea>
+</div>
+<script>
+</script>
+<style>
+  :global(body){
+    background-color: darkslategray;
+  }
+  div{
+    background-color:darkslategray;
+  }
+  h1{
+    text-align: center;
+  }
+  h3{
+    text-align: center;
+  }
+  .redes{
+    display: none;
+    width: 30%;
+    margin: auto;
+  }
+  .command{
+    color:aqua;
+  }
+  .input{
+    margin: auto;
+    display: flex;
+    width: 30%;
+    align-items: center;
+  }
+  span{
+    margin-left: auto;
+  }
+  .input > textarea{
+    resize: none;
+    height: 1rem;
+    width: 70%;
+    margin-right: auto;
+    border: none;
+    background-color: darkslategray;
+  }
+</style>
